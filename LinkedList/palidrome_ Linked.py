5814fd764c226919ba5159344ea8f7726a096c51
shashankshashnank-stack <shashankshashnank@gmail.com>
2026-09-04 19:09:05 +0530

    Add optimized palindrome linked list solution

--- LinkedList/palidrome_ Linked.py
+++ LinkedList/palidrome_ Linked.py
@@ -1,1 +1,41 @@
-Implement palindrome linked list using slow fast pointers
+def isPalindrome(head):
+
+    if head is None or head.next is None:
+        return True
+
+    # Find the end of the first half
+    slow = head
+    fast = head
+
+    while fast.next and fast.next.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    # Reverse the second half
+    second = reverse(slow.next)
+
+    first = head
+
+    # Compare both halves
+    while second:
+        if first.data != second.data:
+            return False
+
+        first = first.next
+        second = second.next
+
+    return True
+
+
+def reverse(head):
+
+    prev = None
+    current = head
+
+    while current:
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+
+    return prev
